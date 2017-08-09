a9767c6e02788fb78d0bb480fb26f2a0c85b1e50
Jenwit_Krit <jenwitkmonz@gmail.com>
2017-08-09 13:14:53 +0530

    Add files via upload

---
+++ EmpDetail.java
@@ -0,0 +1,35 @@
+class Employee {
+    int salary;
+    int empno;
+    String name;
+    
+    Employee(){
+        empno = 123;   
+        salary = 2000;
+        name = "kim";
+    }
+    Employee(int eno,int sal,String nm){
+        empno = eno;
+        salary = sal;
+        name = nm;
+    }
+    
+    void display(){
+        System.out.println("Employee = "+empno);
+        System.out.println("Salary = "+salary);
+        System.out.println("Name = "+name);
+    }
+}
+
+public class EmpDetail {
+    public static void main(String[] args){
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee(124,5000,"Jeong");
+        System.out.println("-------------------------");
+        emp1.display();
+        System.out.println("-------------------------");
+        emp2.display();
+        System.out.println("-------------------------");
+    }
+}
+
